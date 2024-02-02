55c3000426d4960785f85a7d41ab34c10ed72c6e
NourOumy <STG0088@BRUFOR.LAN>
2024-02-02 15:20:18 +0100

    fin exercice

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
-window.addEventListener ("scroll", function (){
+function easyscroll(){
     let elements = document.querySelectorAll('.fadeIn')
     elements.forEach(element => {
         if((window.scrollY + window.innerHeight) > (element.offsetTop +(element.offsetHeight/2))){
          element.classList.add("visible")
          }     
      })
+}
+
+easyscroll()
+
+window.addEventListener ("scroll", function (){
+    easyscroll()
     })

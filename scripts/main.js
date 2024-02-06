6dc4312cddeab60189c0bbfbc91e62a78917fb91
NourOumy <STG0088@BRUFOR.LAN>
2024-02-06 11:29:13 +0100

    ajout effet oliver

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,29 @@ function easyscroll(){
          }     
      })
 }
+function easyscrollRight(){
+    let rightElements = document.querySelectorAll('.elementjsright')
+    rightElements.forEach(rightElement => {
+        if((window.scrollY + window.innerHeight) > (rightElement.offsetTop +(rightElement.offsetHeight/2))){
+            rightElement.classList.add("reveal")
+         }     
+     })
+}
+function easyscrollLeft(){
+    let leftElements = document.querySelectorAll('.elementjsleft')
+    leftElements.forEach(leftElement => {
+        if((window.scrollY + window.innerHeight) > (leftElement.offsetTop +(leftElement.offsetHeight/2))){
+            leftElement.classList.add("reveal")
+         }     
+     })
+}
+
+
 
 easyscroll()
 
 window.addEventListener ("scroll", function (){
     easyscroll()
+    easyscrollRight()
+    easyscrollLeft()
     })
